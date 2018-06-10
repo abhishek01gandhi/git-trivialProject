0f55fa27143a3224240294d44419d7d4616bd8a8
Abhishek Gandhi <geniusabhi01@gmail.com>
2018-06-10 15:26:24 +0530

    Math Project using TDD Approach

---
+++ MathsTestProject1/MathsComponent/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsComponentNameSpace
+{
+    public class MathsComponent
+    {
+        public int Add(int num1,int num2) {
+            return num1+num2;
+        }
+
+        public int Sub(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+    }
+}
---
+++ MathsTestProject1/MathsTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MathsComponentNameSpace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathsTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            MathsComponent obj = new MathsComponent();
+            int result = obj.Add(10, 10);
+            Assert.AreEqual(20, result);
+        }
+
+        [TestMethod]
+        public void TestSubstract()
+        {
+            MathsComponent obj = new MathsComponent();
+            int result = obj.Sub(20, 10);
+            Assert.AreEqual(10, result);
+        }
+    }
+}
